49f4e3dd8cc60c7c633f035b4514c2239413798f
ABWeb-Dev <abradford.webdev@gmail.com>
2022-01-28 22:32:45 -0800

    Starting Point. Many Packages such as Bootstrap, Jquery, Popper are all installed. Let me know if you need or want more.

--- LotusTransformation/Startup.cs
+++ LotusTransformation/Startup.cs
@@ -16,6 +16,8 @@ namespace LotusTransformation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +32,16 @@ namespace LotusTransformation
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}",
+                    defaults: new
+                    {
+                        controller = "Lotus",
+                        action = "Home"
+                    });
             });
+            app.UseStaticFiles();
         }
     }
 }
